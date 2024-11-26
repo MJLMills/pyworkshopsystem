b611ad0af6b7c9040875ce9f543cfcc87a116dcc
mjlmills <mjohnmills@gmail.com>
2024-11-26 16:09:20 -0600

    add missing imports

--- src/computer/sockets/__init__.py
+++ src/computer/sockets/__init__.py
@@ -1,11 +1,19 @@
 from src.computer.sockets.audio.audio_input_socket import AudioInputSocketOne
 from src.computer.sockets.audio.audio_input_socket import AudioInputSocketTwo
+from src.computer.sockets.audio.audio_output_socket import AudioOutputSocketOne
+from src.computer.sockets.audio.audio_output_socket import AudioOutputSocketTwo
+from src.computer.sockets.cv_audio.cv_audio_input_socket import CVAudioInputSocketOne
+from src.computer.sockets.cv_audio.cv_audio_input_socket import CVAudioInputSocketTwo
 from src.computer.sockets.cv_audio.cv_audio_output_socket import CVAudioOutputSocketOne
 from src.computer.sockets.cv_audio.cv_audio_output_socket import CVAudioOutputSocketTwo
 from src.computer.sockets.cv.cv_input_socket import CVInputSocketOne
 from src.computer.sockets.cv.cv_input_socket import CVInputSocketTwo
+from src.computer.sockets.cv.cv_output_socket import CVOutputSocketOne
+from src.computer.sockets.cv.cv_output_socket import CVOutputSocketTwo
 from src.computer.sockets.pulses.pulse_input_socket import PulseInputSocketOne
 from src.computer.sockets.pulses.pulse_input_socket import PulseInputSocketTwo
+from src.computer.sockets.pulses.pulse_output_socket import PulseOutputSocketOne
+from src.computer.sockets.pulses.pulse_output_socket import PulseOutputSocketTwo
 
 
 class Socket(object):
