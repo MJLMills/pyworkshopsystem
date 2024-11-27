83d1bb7496e0e8d34b85845e04554b384d7d505a
mjlmills <mjohnmills@gmail.com>
2024-11-27 13:22:55 -0600

    add read method to get the range of the sockets over N samples

--- src/computer/sockets/audio/audio_input_sockets.py
+++ src/computer/sockets/audio/audio_input_sockets.py
@@ -1,3 +1,4 @@
+import array
 from .audio_input_socket import AudioInputSocketOne
 from .audio_input_socket import AudioInputSocketTwo
 
@@ -12,3 +13,16 @@ class AudioInputSockets(object):
     def read(self):
         return (self.socket_one.read(),
                 self.socket_two.read())
+
+    def read_n(self, n):
+
+        left_values = array.array(typecode="I") * n
+        right_values = array.array(typecode="I") * n
+
+        for i in range(0, n):
+            left_values[i] = self.socket_one.read()
+            right_values[i] = self.socket_two.read()
+
+        return (max(left_values) - min(left_values),
+                max(right_values) - min(right_values))
+
