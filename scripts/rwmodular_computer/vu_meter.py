722bf0af1fecfd8bfb2ba2b05a61b40c68930e4f
mjlmills <mjohnmills@gmail.com>
2024-11-27 14:17:10 -0600

    add docstring and inline comments

--- scripts/rwmodular_computer/vu_meter.py
+++ scripts/rwmodular_computer/vu_meter.py
@@ -10,7 +10,18 @@ n_samples_per_iteration = 10
 audio_limits = (2000, 8000, 20000)
 
 def audio_value_to_num_leds(value):
+    """Convert an audio range value to a number of LEDs.
 
+    Parameters
+    ----------
+    value : int
+        The audio range value to convert.
+
+    Returns
+    -------
+    int
+        The number of LEDs to light.
+    """
     if value < audio_limits[0]:
         return 0
     elif value in range(audio_limits[0], audio_limits[1]):
@@ -20,12 +31,15 @@ def audio_value_to_num_leds(value):
     elif value >= audio_limits[2]:
         return 3
 
+# instantiate the computer
 computer = Computer()
+# get a reference to the CV/audio input socket pair
 cv_audio_input_sockets = computer.cv_audio_input_sockets
+# get a reference to the LED matrix
 led_matrix = computer.led_matrix
 
 while True:
-    # read the range of values from the left and right CV/audio sockets over n samples
+    # read the range of values from the left and right CV/audio sockets over a number of samples
     left_audio_value, right_audio_value = cv_audio_input_sockets.read_range(num_samples=n_samples_per_iteration)
 
     # turn off the matrix
