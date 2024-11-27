e53b9696c0d07fd6c98a4bc16209f229c0c9b9ae
mjlmills <mjohnmills@gmail.com>
2024-11-26 20:53:46 -0600

    instantiate the ADC only once

--- src/computer/switches/switch_z.py
+++ src/computer/switches/switch_z.py
@@ -10,12 +10,13 @@ class SwitchZ(object):
 
     def __init__(self, multiplexer):
         self.__multiplexer = multiplexer
+        self.__adc = machine.ADC(self.__multiplexer.MUX_IO_PIN_ONE_ID)
 
     def read(self):
         self.__multiplexer.set_logic_pin_values(self.MUX_LOGIC_A_PIN_VALUE,
                                                 self.MUX_LOGIC_B_PIN_VALUE)
 
-        return machine.ADC(self.__multiplexer.MUX_IO_PIN_ONE_ID).read_u16()
+        return self.__adc.read_u16()
 
     def is_on(self):
         return self.read() == 1
