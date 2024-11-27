eb68d9d8db53ac6a0c49a6231b67be5d6029050a
mjlmills <mjohnmills@gmail.com>
2024-11-27 13:21:55 -0600

    add a simple read method

--- src/computer/sockets/audio/audio_input_socket.py
+++ src/computer/sockets/audio/audio_input_socket.py
@@ -1,6 +1,7 @@
 from src.computer.sockets import InputSocket
 import machine
 # TODO - enable L/R syntax here too
+# TODO - needs a read method
 
 class AudioInputSocket(InputSocket):
     """
@@ -10,9 +11,11 @@ class AudioInputSocket(InputSocket):
     -6V = 4095
     DC coupled, requires calibration for precise readings.
     """
-
     def __init__(self, number):
         self.adc = machine.ADC(number)
+
+    def read(self):
+        raise self.adc.read_u16()
 
 
 class AudioInputSocketOne(AudioInputSocket):
