8ed038bd2e49a2f1e028114ad3748a16f300d852
mjlmills <mjohnmills@gmail.com>
2024-11-29 11:26:03 -0600

    improve docstring

--- src/computer/io.py
+++ src/computer/io.py
@@ -2,7 +2,12 @@ from abc import ABC, abstractmethod
 
 
 class IO(ABC):
-    """An object with an associated GPIO pin."""
+    """An object with an associated GPIO pin.
+
+    All hardware objects have a single associated GPIO pin,
+    regardless of whether they use that pin directly or through
+    the multiplexer (in which case the GPIO pin may be shared).
+    """
     @property
     @abstractmethod
     def pin_id(self):
