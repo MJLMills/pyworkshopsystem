81f6aa73fc99401457c9a46bb6bdc57371c7847c
mjlmills <mjohnmills@gmail.com>
2024-11-29 09:56:34 -0600

    remove comment on knobs

--- src/computer/knobs/__init__.py
+++ src/computer/knobs/__init__.py
@@ -1,6 +1,3 @@
 from .main_knob import MainKnob
 from .knob_x import KnobX
 from .knob_y import KnobY
-
-
-# all knobs should have settable ranges and units, i.e. are normalized (with default being no normalization)
