9894aaf8d91a03d042aa7864b0db396fd8e2e75c
mjlmills <mjohnmills@gmail.com>
2024-11-29 11:08:45 -0600

    add top-level init contents

--- src/computer/__init__.py
+++ src/computer/__init__.py
@@ -1,1 +1,8 @@
+"""
+pyworkshopsystem
+----------------
+
+This package is the top-level and provides the Computer class, which
+itself provides all the hardware component classes as members.
+"""
 from .computer import Computer
