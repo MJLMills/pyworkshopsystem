ee8685f44e33c703fa6c40249ae504c40ff98966
mjlmills <mjohnmills@gmail.com>
2024-12-16 22:10:25 -0600

    correct range and inversion of PWM outputs on CV sockets

--- src/computer/sockets/cv/cv_output_socket.py
+++ src/computer/sockets/cv/cv_output_socket.py
@@ -5,28 +5,35 @@ from multiplexed_input import IO
 class CVOutputSocket(IO):
     """The CV output sockets of the Computer.
 
+    These sockets use PWM output
+
     Inverted PWM output.
     Two-pole active filtered. Use 11-bit PWM at 60 kHz.
-    2047 = -6V
-    1024 = 0V
+
+    The duty cycle is a 16-bit unsigned integer.
+
+    65535 = -6V
+    32768 = 0V
     0 = +6V
+
     Requires firmware calibration for precise values.
     """
-    DUTY_U16 = 32768
+    FREQUENCY_KHZ = 60000
 
-    def __init__(self):
+    def __init__(self, duty_cycle: int = 32768):
+
         self.pwm = PWM(self.pin_id,
                        freq=60000,
-                       duty_u16=CVOutputSocket.DUTY_U16)
+                       duty_u16=duty_cycle)
 
     def write(self, value):
-        self.pwm.duty_u16(value)
+        self.pwm.duty_u16(65535 - value)
 
     def write_norm_value(self, value):
-        if 0.0 <= value <= 1.0:
-            raise ValueError
+        if not (0.0 <= value <= 1.0):
+            print("Normalized input exceeded range:", value)
         
-        self.write(value * CVOutputSocket.DUTY_U16)
+        self.write((1.0 - value) * CVOutputSocket.DEFAULT_DUTY_CYCLE)
 
 
 class CVOutputSocketOne(CVOutputSocket):
