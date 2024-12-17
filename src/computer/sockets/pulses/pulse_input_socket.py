f91d00678664e4bef47442a753fa1af02485dc36
mjlmills <mjohnmills@gmail.com>
2024-12-17 16:58:24 -0600

    add other doc notes

--- src/computer/sockets/pulses/pulse_input_socket.py
+++ src/computer/sockets/pulses/pulse_input_socket.py
@@ -7,6 +7,11 @@ class PulseInputSocket(IO):
     Inverted digital input: Low input = High reading.
     For example, use a falling edge to track the start of a pulse.
     NB: Input pin must have the pull-up enabled, this powers the transistor.
+
+    Simple digital on/off signals, buffered and scaled with transistors.
+    Use them for clocks, pulses, gates. They could also produce unfiltered PWM signals, so could maybe be used for gnarly audio (loud!) or gritty CV.
+    They’ll often be used to trigger the envelopes, which are Serge-style voltage controlled slopes
+    The gates are about 5-6v
     """
     def __init__(self):
 
