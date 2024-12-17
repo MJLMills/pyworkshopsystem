82fb5d1621b9a667ff72893d2f2e2173e4e342ae
mjlmills <mjohnmills@gmail.com>
2024-12-17 09:43:31 -0600

    add calibrated boundaries for switch read

--- src/computer/switches/switch_z.py
+++ src/computer/switches/switch_z.py
@@ -6,30 +6,35 @@ class SwitchZ(MultiplexedInput):
 
     The switch has three states:
 
-    Up - latching, high value on read.
-    Middle - latching, medium value on read.
-    Down - momentary, low value on read.
+    Up - latching, high value on read - always 65535
+    Middle - latching, medium value on read - ranges 32311 to 32407 over 200 secs (converged after 140 secs)
+    Down - momentary, low value on read - ranges 176 to 272 over 200 secs (converged after 4 secs)
     """
+
+    DOWN_MID_BOUNDARY = 16292
+    MID_UP_BOUNDARY = 48971
+    UP_MAX = 65535
+
     @property
-    def mux_logic_a_pin_value(self):
+    def mux_logic_a_pin_value(self) -> int:
         return 1
 
     @property
-    def mux_logic_b_pin_value(self):
+    def mux_logic_b_pin_value(self) -> int:
         return 1
 
     @property
     def pin_id(self) -> int:
         return 28
 
-    def is_up(self) -> bool:
-        return self.read() > 50
-
     def is_down(self) -> bool:
-        return self.read() < 10
+        """Determine whether the switch is down."""
+        return 0 <= self.read() < SwitchZ.DOWN_MID_BOUNDARY
 
     def is_middle(self) -> bool:
-        if not self.is_up() and not self.is_down():
-            return True
-        else:
-            return False
+        """Determine whether the switch is in the middle."""
+        return SwitchZ.DOWN_MID_BOUNDARY <= self.read() < SwitchZ.MID_UP_BOUNDARY
+
+    def is_up(self) -> bool:
+        """Determine whether the switch is up."""
+        return SwitchZ.MID_UP_BOUNDARY <= self.read() <= SwitchZ.UP_MAX
