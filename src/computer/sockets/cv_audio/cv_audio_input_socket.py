e5f5292c0810c78fc3afb3fae10311b48b802a11
mjlmills <mjohnmills@gmail.com>
2024-12-18 21:52:56 -0600

    move documentation to class

--- src/computer/sockets/cv_audio/cv_audio_input_socket.py
+++ src/computer/sockets/cv_audio/cv_audio_input_socket.py
@@ -5,11 +5,23 @@ from input_output import IO
 class CVAudioInputSocket(IO):
     """The CV/Audio input sockets of the computer.
 
-    Inverted bipolar analog input, into 12(?) bit internal ADC.
-    +6V = 0
-    0V = 2048
-    -6V = 4095
-    DC coupled, requires calibration for precise readings.
+    The CV/Audio analog inputs are bipolar (inverted) and DC-coupled.
+    They require calibration for precise readings.
+    The values returned by machine.ADC.read_u16 are in the range:
+    +6v = 0
+     0v = 32768
+    -6v = 65535
+    In practice the precision of the internal ADC is closer to 12-bit.
+
+    The left input is normalled to the right input, so if only one
+    socket is plugged in, both channels receive the same input.
+
+    The input signals are scaled (presumably in hardware) and then
+    go to channels 0 and 1 of an internal analog to digital converter
+    (Note that the multiplexer makes use of channels 3 and 4 to read
+    the CV inputs, knobs and switch). From there they appear to go to
+    two assigned GPIO pins (26 and 27) on the Pi, from which they are
+    directly readable as analog inputs.
     """
     def __init__(self):
         self._adc = machine.ADC(self.pin_id)
