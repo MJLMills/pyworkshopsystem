fd5097ce06a26633722ee6f34a128b10c15435d8
mjlmills <mjohnmills@gmail.com>
2024-12-18 21:36:56 -0600

    change the CV/Audio inputs to reflect that they are directly read off the GPIO pins

--- src/computer/sockets/cv_audio/cv_audio_input_socket.py
+++ src/computer/sockets/cv_audio/cv_audio_input_socket.py
@@ -1,35 +1,36 @@
-from abc import ABC
 import machine
-from multiplexed_input import IO
+from input_output import IO
 
 
-class CVAudioInputSocket(ABC, IO):
-    """
+class CVAudioInputSocket(IO):
+    """The CV/Audio input sockets of the computer.
+
     Inverted bipolar analog input, into 12(?) bit internal ADC.
     +6V = 0
     0V = 2048
     -6V = 4095
     DC coupled, requires calibration for precise readings.
     """
-    def __init__(self, number):
-        self.adc = machine.ADC(number)
+    def __init__(self):
+        self._adc = machine.ADC(self.pin_id)
 
     def read(self):
-        raise self.adc.read_u16()
+        return self._adc.read_u16()
+
+    def read_norm(self):
+        return self.read() / 65535
 
 
 class CVAudioInputSocketOne(CVAudioInputSocket):
-    """The left audio input socket."""
-
-    PIN_ID = 26
-
-    def __init__(self):
-        super().__init__(number=1)
+    """The left CV/Audio input socket."""
+    @property
+    def pin_id(self):
+        return 26
 
 
 class CVAudioInputSocketTwo(CVAudioInputSocket):
-    """The right audio input socket."""
-    PIN_ID = 27
+    """The right CV/Audio input socket."""
 
-    def __init__(self):
-        super().__init__(number=2)
+    @property
+    def pin_id(self):
+        return 27
