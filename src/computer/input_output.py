58bd540c7472341b603dd31d3c1b711c134abc30
mjlmills <mjohnmills@gmail.com>
2024-12-26 13:45:42 -0600

    update docstring, line-spacing and the raised error message.

--- src/computer/input_output.py
+++ src/computer/input_output.py
@@ -1,10 +1,14 @@
 class IO(object):
-    """An object with an associated GPIO pin.
+    """A hardware object with an associated GPIO pin.
 
-    All hardware objects have a single associated GPIO pin,
-    regardless of whether they use that pin directly or through
-    the multiplexer (in which case the GPIO pin may be shared).
+    All hardware objects (other than the CV/Audio output sockets) have a single
+    associated GPIO pin, regardless of whether they use that pin directly or
+    through the multiplexer (in which case the GPIO pin may be shared). This
+    pin has a unique integer identifier.
     """
     @property
-    def pin_id(self):
-        raise NotImplementedError
+    def pin_id(self) -> int:
+        """The unique identifier of the GPIO pin used by this class."""
+        raise NotImplementedError(
+            self.__class__.__name__ + " does not implement pin_id property."
+        )
