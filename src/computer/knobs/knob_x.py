32d0593f59675d3cc4f03c62ff76912c6ee32dc3
mjlmills <mjohnmills@gmail.com>
2024-12-26 20:30:21 -0600

    add methods from od.py

--- src/computer/knobs/knob_x.py
+++ src/computer/knobs/knob_x.py
@@ -4,6 +4,23 @@ from multiplexed_input import MultiplexedInput
 class KnobX(MultiplexedInput):
     """The knob marked X."""
 
+    def __init__(self, max_value=None, min_value=None):
+
+        super().__init__()
+
+        # redundant but depends on normalization
+        if max_value is None:
+            self._max_value = 65535
+        else:
+            self._max_value = max_value
+
+        if min_value is None:
+            self._min_value = 224
+        else:
+            self._min_value = min_value
+
+        self._range = self._max_value - self._min_value
+
     @property
     def pin_id(self) -> int:
         """The unique identifier of the GPIO pin used by this class."""
@@ -12,9 +29,12 @@ class KnobX(MultiplexedInput):
     @property
     def mux_logic_a_pin_value(self) -> bool:
         """The value of the first multiplexer login pin for this input."""
-        return 1
+        return True
 
     @property
     def mux_logic_b_pin_value(self) -> bool:
         """The value of the second multiplexer login pin for this input."""
-        return 0
+        return False
+
+    def read_norm(self):
+        return (self.read() - self._min_value) / self._range
