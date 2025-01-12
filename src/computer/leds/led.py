cef27c93e5573003774fb319f4c817e935e5d3f6
mjlmills <mjohnmills@gmail.com>
2025-01-11 21:45:17 -0600

    update from latest main.py

--- src/computer/leds/led.py
+++ src/computer/leds/led.py
@@ -1,8 +1,9 @@
+import time
 import machine
-from multiplexed_input import IO
+from input_output import HardwareComponent
 
 
-class LED(IO):
+class LED(HardwareComponent):
     """A light emitting diode on the module.
 
     If the pin value is set to 1 (i.e. True), the LED is illuminated.
@@ -45,6 +46,12 @@ class LED(IO):
     def turn_off(self):
         self.pin.value(0)
 
+    def toggle(self):
+        if self.value == 1:
+            self.turn_off()
+        elif self.value == 0:
+            self.turn_on()
+
     @property
     def value(self):
         return self.pin.value()
@@ -52,3 +59,12 @@ class LED(IO):
     @value.setter
     def value(self, value):
         self.pin.value(value)
+
+    def set_value(self, value):
+        self.pin.value(value)
+
+    def pulse(self, time_s):
+        self.toggle()
+        time.sleep(time_s)
+        self.toggle()
+
