26798dab87d530435a300d10f13c22672dcd2f00
mjlmills <mjohnmills@gmail.com>
2025-01-12 21:22:05 -0600

    update knobs to have key variables stored in class vars instead of properties

--- src/computer/knobs/knob_y.py
+++ src/computer/knobs/knob_y.py
@@ -3,29 +3,34 @@ from base.multiplexed_input import MultiplexedInput
 
 class KnobY(MultiplexedInput):
     """The knob marked Y."""
+    __IO_PIN_ID = 28
+    __MIN_VALUE_U16 = 224
+    __MAX_VALUE_U16 = 65535
+    __MUX_LOGIC_A_PIN_VALUE = False
+    __MUX_LOGIC_B_PIN_VALUE = True
 
     def __init__(self):
         super().__init__()
 
     @property
+    def pin_id(self) -> int:
+        """The unique identifier of the GPIO pin used by this class."""
+        return self.__IO_PIN_ID
+
+    @property
     def min_value(self) -> int:
-        return 224
+        return self.__MIN_VALUE_U16
 
     @property
     def max_value(self) -> int:
-        return 65535
-
-    @property
-    def pin_id(self) -> int:
-        """The unique identifier of the GPIO pin used by this class."""
-        return 28
+        return self.__MAX_VALUE_U16
 
     @property
     def mux_logic_a_pin_value(self) -> bool:
         """The value of the first multiplexer login pin for this input."""
-        return False
+        return self.__MUX_LOGIC_A_PIN_VALUE
 
     @property
     def mux_logic_b_pin_value(self) -> bool:
         """The value of the second multiplexer login pin for this input."""
-        return True
+        return self.__MUX_LOGIC_B_PIN_VALUE
