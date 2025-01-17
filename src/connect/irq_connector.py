76452caa7ae45f8d79fdaad3e12877336bb16420
mjlmills <mjohnmills@gmail.com>
2025-01-17 08:41:12 -0600

    copy IRQ connector code from od module to here

--- src/connect/irq_connector.py
+++ src/connect/irq_connector.py
@@ -41,3 +41,105 @@ class LEDFlasher(IRQConnector):
         # if this can just set a bool to be consumed by something in a loop that might help.
         # but then why not just poll the value? - figure this out.
         # see set_irq below:
+
+
+
+import machine
+from looper import Looper
+
+# TODO - these are technically signals and slots, the IRQ firing is a signal
+# and the callback(s) it calls is a slot
+# see if these can be made syntactically the same as signal/slots
+# e.g. the callback could just emit a signal then the code can register slots
+# to that signal
+# this code can probably be housed in the DigitalInput classes (pulse inputs)
+# since the IRQ is set on the pulse input pin.
+# check if a pin can have multiple IRQs
+
+class IRQConnector(object):
+    """Connect an IRQ source to an output.
+
+    For pins on which an IRQ can be set, this class can be used to add a
+    callback that has access to the necessary program state without defining
+    global variables.
+
+    Parameters
+    ----------
+    irq_source
+        The pin on which the IRQ is to be set.
+    outputs
+        The class instances providing access to program state.
+    """
+    def __init__(self,
+                 irq_source: machine.Pin,
+                 outputs: list,
+                 trigger):
+
+        self._outputs = outputs
+        irq_source.pin.irq(handler=self.callback,
+                           trigger=trigger)
+
+    def callback(self, irq_source_pin):
+        raise NotImplementedError(
+            "Connector subclasses must define a callback method.")
+
+
+class ToggleLooping(IRQConnector):
+    """Connect a pulse input to toggle looping of the system.
+
+    This class sets an IRQ on a provided digital input pin which toggles the
+    provided looper on or off (depending on its current state) when the pin
+    goes high.
+
+    Parameters
+    ----------
+    pulse_input
+        The pulse input that will toggle looping.
+    looper
+        The looper whose state will be toggled by the pulse input.
+    """
+
+    def __init__(self,
+                 pulse_input: machine.Pin,
+                 looper: Looper):
+        super().__init__(pulse_input,
+                         outputs=[looper],
+                         trigger=machine.Pin.IRQ_RISING)
+
+        self._looper = self._outputs[0]
+
+    def callback(self, _):
+        """Toggle looping when the pulse input goes high."""
+        self._looper.toggle_looping()
+
+
+# if the start point is pinged, tell the looper (set_initial_coordinates)
+class SetStartPoint(IRQConnector):
+    """Connect a pulse input one to set the start point of a looper.
+
+    This class sets an IRQ on a provided digital input pin which sets the start
+    point of the provided looper when the pin goes high.
+
+    Parameters
+    ----------
+    pulse_input
+        The pulse input that will set the start point.
+    looper
+        The looper whose start point will be set by the pulse input.
+    """
+
+    def __init__(self,
+                 pulse_input: machine.Pin,
+                 looper: Looper,
+                 led: LED):
+        super().__init__(pulse_input,
+                         outputs=[looper, led],
+                         trigger=machine.Pin.IRQ_RISING)
+
+        self._looper = self._outputs[0]
+        self._led = self._outputs[1]
+
+    def callback(self, irq_source_pin):
+        """Set the start point when the pin goes high."""
+        self._looper.set_initial_coordinates()
+        self._led.pulse(0.001)
