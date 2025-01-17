1c93323d5242a0c7b00e67832d4a6b824d51fed8
mjlmills <mjohnmills@gmail.com>
2025-01-17 12:35:35 -0600

    add private class variables for abstract properties, add class variables instead of hard-coded constants, remove pin property

--- src/computer/sockets/pulses/pulse_input_socket.py
+++ src/computer/sockets/pulses/pulse_input_socket.py
@@ -11,6 +11,9 @@ class PulseInputSocket(HardwareComponent):
 
     The gates are about 5-6v
     """
+    __ON_VALUE = 0
+    __OFF_VALUE = 1
+
     def __init__(self):
 
         self._pin = machine.Pin(self.io_pin_id,
@@ -25,10 +28,6 @@ class PulseInputSocket(HardwareComponent):
     def __emit_pulse_started(self, _):
         self.pulse_started.emit()
 
-    @property
-    def pin(self):
-        return self._pin
-
     def set_irq(self, handler):
         self._pin.irq(handler=handler, trigger=machine.Pin.IRQ_FALLING)
 
@@ -36,25 +35,27 @@ class PulseInputSocket(HardwareComponent):
         return self._pin.value()
 
     def is_high(self):
-        return self.read() == 0
+        return self.read() == self.__ON_VALUE
 
     def is_low(self):
-        return self.read() == 1
+        return self.read() == self.__OFF_VALUE
 
 
 class PulseInputSocketOne(PulseInputSocket):
     """The first (leftmost) pulse input socket."""
+    __IO_PIN_ID = 2
 
     @property
     def io_pin_id(self):
         """The unique identifier of the GPIO pin used by this class."""
-        return 2
+        return self.__IO_PIN_ID
 
 
 class PulseInputSocketTwo(PulseInputSocket):
     """The second (rightmost) pulse input socket."""
+    __IO_PIN_ID = 3
 
     @property
     def io_pin_id(self):
         """The unique identifier of the GPIO pin used by this class."""
-        return 3
+        return self.__IO_PIN_ID
