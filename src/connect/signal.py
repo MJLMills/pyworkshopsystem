74a6b9b56729ee60792c0eb429a05b8194ef408e
mjlmills <mjohnmills@gmail.com>
2025-02-06 21:08:34 -0600

    add docstrings, allow disconnect of multiple slots, add slots to str rep

--- src/connect/signal.py
+++ src/connect/signal.py
@@ -1,21 +1,41 @@
 class Signal(object):
-
+    """A signal emitted by an object instance."""
     def __init__(self):
-        self.slots = []
+        self._slots = []
 
     def connect(self, *slots):
+        """Connect the specified slots to this signal.
+
+        Parameters
+        ----------
+        slots : list of callable
+            The callable slots to connect to this signal.
+        """
         for slot in slots:
-            if slot not in self.slots:
-                self.slots.append(slot)
+            if slot not in self._slots:
+                self._slots.append(slot)
 
-    def disconnect(self, slot):
-        if slot in self.slots:
-            self.slots.pop(self.slots.index(slot))
+    def disconnect(self, *slots):
+        """Disconnect the specified slots from this signal.
 
-    def emit(self, **kwargs):
-        for slot in self.slots:
+        Parameters
+        ----------
+        slots : list of callable
+            The callable slots to disconnect from this signal.
+        """
+        for slot in slots:
+            if slot in self._slots:
+                self._slots.pop(self._slots.index(slot))
+
+    def emit(self, **kwargs) -> None:
+        """Call each slot connected to this signal."""
+        for slot in self._slots:
             slot(**kwargs)
 
-    def __str__(self):
-        str_rep = self.__class__.__name__
-        return str_rep
+    def __str__(self) -> str:
+        """Create a human-readable representation of this object."""
+        str_rep = self.__class__.__name__ + ", slots = ["
+        for slot in self._slots:
+            str_rep += str(slot) + ", "
+
+        return str_rep[:-2]
