bdbc6ffc6216b7825ca183970179584fd35ed9c3
MJLMills <mjohnmills@gmail.com>
2026-07-24 13:45:21 -0500

    make on_value and off_value class constants instead of properties

--- src/computer/leds/led.py
+++ src/computer/leds/led.py
@@ -1,3 +1,4 @@
+from micropython import const
 from computer.base.digital_output import DigitalOutput
 
 
@@ -6,21 +7,14 @@ class LED(DigitalOutput):
 
     If the pin value is set to 1 (i.e. True), the LED is illuminated.
 
-    Methods
-    -------
-    turn_on
-        Turn this LED on.
-    turn_off
-        Turn this LED off.
-
     Properties
     ----------
     value
         The value of this LED.
     """
-    __ON_VALUE = 1
-    __OFF_VALUE = 0
-    __FIRST_LED_PIN_INDEX = 10
+    ON_VALUE = const(1)
+    OFF_VALUE = const(0)
+    __FIRST_LED_PIN_INDEX = const(10)
 
     def __init__(self, led_index):
         if led_index not in range(1, 7):
@@ -33,13 +27,3 @@ class LED(DigitalOutput):
     def io_pin_id(self) -> int:
         """The unique identifier of the GPIO pin used by this class."""
         return self._pin_id
-
-    @property
-    def on_value(self) -> int:
-        """The value used to represent "on" for this digital output."""
-        return self.__ON_VALUE
-
-    @property
-    def off_value(self) -> int:
-        """The value used to represent "off" for this digital output."""
-        return self.__OFF_VALUE
