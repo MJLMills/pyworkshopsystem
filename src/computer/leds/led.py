dad5cd95e70bae477f5c20eddfd700bcf3dfcc4d
MJLMills <mjohnmills@gmail.com>
2026-07-24 14:46:40 -0500

    micro-optimize the LED class

--- src/computer/leds/led.py
+++ src/computer/leds/led.py
@@ -6,22 +6,24 @@ class LED(DigitalOutput):
     """A light emitting diode on the module.
 
     If the pin value is set to 1 (i.e. True), the LED is illuminated.
+    The LED pins start at 10 and end at 15. Boundary checks and pin ID
+    calculations use hardcoded values.
 
-    Properties
+    Parameters
     ----------
-    value
-        The value of this LED.
+    led_index: int
+        The index (1 to 6) of the LED.
+
     """
     ON_VALUE = const(1)
     OFF_VALUE = const(0)
-    FIRST_LED_PIN_INDEX = const(10)
 
     def __init__(self, led_index):
-        if led_index not in range(1, 7):
-            raise ValueError("Invalid LED index: ", led_index)
+        if not 1 <= led_index <= 6:
+            raise ValueError(f"Invalid LED index: {led_index}")
 
-        self.io_pin_id = self.FIRST_LED_PIN_INDEX + (led_index - 1)
+        self.__io_pin_id = 9 + led_index
         super().__init__()
 
-    def _resolve_pin_id(self):
-        return self.io_pin_id
+    def _resolve_pin_id(self) -> int:
+        return self.__io_pin_id
