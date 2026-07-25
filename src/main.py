5ccc43d08167650b25e057bf81bc54a24de50ec8
MJLMills <mjohnmills@gmail.com>
2026-07-25 13:44:07 -0500

    update flashing main.py to turn off LEDs on keyboard interrupt

--- src/main.py
+++ src/main.py
@@ -14,21 +14,23 @@ led_six = led_matrix.get_by_index(6)
 
 sleep_time_seconds = 0.2
 sleep = time.sleep
-
-while True:
-    led_matrix.toggle()
-    sleep(sleep_time_seconds)
-    led_matrix.toggle()
-    sleep(sleep_time_seconds)
-    led_one.toggle()
-    sleep(sleep_time_seconds)
-    led_two.toggle()
-    sleep(sleep_time_seconds)
-    led_three.toggle()
-    sleep(sleep_time_seconds)
-    led_four.toggle()
-    sleep(sleep_time_seconds)
-    led_five.toggle()
-    sleep(sleep_time_seconds)
-    led_six.toggle()
-    sleep(sleep_time_seconds)
+try:
+    while True:
+        led_matrix.toggle()
+        sleep(sleep_time_seconds)
+        led_matrix.toggle()
+        sleep(sleep_time_seconds)
+        led_one.toggle()
+        sleep(sleep_time_seconds)
+        led_two.toggle()
+        sleep(sleep_time_seconds)
+        led_three.toggle()
+        sleep(sleep_time_seconds)
+        led_four.toggle()
+        sleep(sleep_time_seconds)
+        led_five.toggle()
+        sleep(sleep_time_seconds)
+        led_six.toggle()
+        sleep(sleep_time_seconds)
+except KeyboardInterrupt:
+    led_matrix.turn_off()
