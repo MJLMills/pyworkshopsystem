0aae94d318697656416a7ed1b3aa73534d88f219
MJLMills <mjohnmills@gmail.com>
2026-07-25 14:21:49 -0500

    update main.py to include fading of LED matrix

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import time
-from computer import Computer
+from computer.leds import LEDMatrix
 
-computer = Computer()
-led_matrix = computer.led_matrix
-led_matrix.turn_off()
+sleep_time_seconds = 0.25
+gamma_table_size = 1024
+fade_step_length_ms = 10
+sleep = time.sleep
+
+led_matrix = LEDMatrix(start_value=0, table_size=gamma_table_size)
 
 led_one = led_matrix.get_by_index(1)
 led_two = led_matrix.get_by_index(2)
@@ -12,8 +15,6 @@ led_four = led_matrix.get_by_index(4)
 led_five = led_matrix.get_by_index(5)
 led_six = led_matrix.get_by_index(6)
 
-sleep_time_seconds = 0.2
-sleep = time.sleep
 try:
     while True:
         led_matrix.toggle()
@@ -32,5 +33,19 @@ try:
         sleep(sleep_time_seconds)
         led_six.toggle()
         sleep(sleep_time_seconds)
+
+        led_matrix.turn_off()
+        led_matrix.use_pwm_mode()
+        for b in range(0, 65536, gamma_table_size):
+            led_matrix.set_matrix_brightness(b)
+            time.sleep_ms(fade_step_length_ms)
+        for b in range(65535, -1, -gamma_table_size):
+            led_matrix.set_matrix_brightness(b)
+            time.sleep_ms(fade_step_length_ms)
+
+        led_matrix.use_digital_mode()
+        led_matrix.turn_off()
+
 except KeyboardInterrupt:
+    led_matrix.use_digital_mode()
     led_matrix.turn_off()
